bb93260e9f3a6dda2a0575b33b7db757129490f8
Kevin Real Alejo <kevin.real@verychic.com>
2025-06-19 20:09:44 +0200

    feat: add exercice 6

---
+++ src/exercice6.ts
@@ -0,0 +1,135 @@
+/*
+Ejercicio 6
+
+Estás trabajando en un ecommerce y tienes que programar el módulo del checkout. En este ejercicio no puedes usar interface. 
+Recuerda dejar todo preparado para que en un futuro las id puedan cambiar de number a string sin tener que modificar mucho código.
+
+Te viene dado un array de categorías en forma de strings. Este listado de categorías no va a cambiar nunca.
+
+const categories = ["children", "adults", "pets"];
+
+Crea un tipo CategoriesProducts para tipar un objeto así:
+
+const categoriesProducts: CategoriesProducts = {
+  adults: 33,
+  children: 21,
+  pets: 18,
+};
+
+Crea un tipo Product que tenga:
+
+    id
+    nombre
+    categoría
+    precio
+    unidades en stock
+
+Crea un tipo PreOrder que tenga:
+
+    id de cliente
+    fecha
+    productos (opcional)
+    importe total (opcional)
+
+En el campo productos se debe almacenar un array de objetos que tengan esta información:
+
+    id de producto
+    unidades
+
+Crea un tipo Order que tenga todas las propiedades de PreOrder pero obligatorias, con
+un par de datos extra: la id y la dirección de entrega del pedido.
+
+Crea un tipo ClientsOrdersAddresses para un objeto donde cada key será una id de cliente. En cada key tiene que haber un objeto cuya key será una id de pedido,
+ y en cada una de estas keys tiene que haber un objeto con los productos del pedido y su dirección. Servirá para tipar un objeto así:
+
+const clientsOrdersAddresses = {
+  13: {  // id de cliente
+    45: {  // id de pedido
+      products: [{...}, {...}], // array con los productos del pedido
+      address: "..." // la dirección del pedido
+    },
+    67: {
+      products: [{...}, {...}],
+      address: "..."
+    }
+  },
+  15: {
+    ...
+  }
+}
+
+Una vez que tengas todos los tipos, crea las siguientes funciones:
+
+addLineToPreOrder: recibe un pedido que puede ser Order o PreOrder. Si lo recibido es un pedido completo, debe lanzar un error. También recibe un objeto con la id del
+ producto a añadir y las unidades.
+
+processOrder: recibe un pedido que puede ser Order o PreOrder. Si lo recibido no es un pedido completo, debe lanzar un error. Si es un pedido completo, debe consolear sus datos.
+
+increaseTotalProductsInCategory: recibe un objeto categoriesProducts, un nombre de categoría (de las existentes en ese objeto) y una cantidad, y le incrementa a esa categoría la 
+cantidad recibida.
+
+getOrderAddress: recibe un objeto clientsOrdersAddresses, una id de cliente y una id de pedido, y tiene que devolver la dirección de entrega.
+*/
+
+
+const categories = ["children", "adults", "pets"] as const;
+
+type Id = string | number
+
+type Client = {
+    id: number,
+    name: string,
+    adress: string
+}
+
+type CategoriesProducts = {
+    adults: number,
+    children: number,
+    pets: number,
+}
+
+type Product = {
+    id: Id,
+    name: string,
+    category: CategoriesProducts,
+    price: string
+    stock: number,
+}
+
+type PreOrderProduct = Pick<Product, 'id' | 'stock'>
+
+type PreOrder = {
+    clientId: Client['id'],
+    date: Date,
+    products?: PreOrderProduct[],
+    total?: string
+}
+
+type Order = Required<PreOrder> & {
+    id: Id,
+    address: string    
+}
+
+type ClientOrderAdress = Record<Id, Record<Id, {products:PreOrderProduct[], address:string }>>
+
+
+function addLineToPreOrder(order: Order, productId: Id, cuantities: number): void
+function addLineToPreOrder(order: PreOrder, productId: Id, cuantities: number): void
+function addLineToPreorder(order: Order | PreOrder, productId: Id, cuantities: number): {productId: Id, cuantities: number} {
+
+    if(typeof order === ) { throw new Error }
+    return {productId, cuantities}
+}
+
+
+function processOrder(): void {
+
+}
+
+function increaseTotalProductsInCategory(): void {
+
+}
+
+function getOrderAddress(): void {
+
+}
